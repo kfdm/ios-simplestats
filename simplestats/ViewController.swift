77dafae2f22288a2146cf8bbbc4c154df1607082
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-07-08 15:13:17 +0900

    Initial Commit

---
+++ simplestats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simplestats
+//
+//  Created by Paul Traylor on 2017/07/08.
+//  Copyright © 2017年 Paul Traylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
