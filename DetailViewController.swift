7eeeda4b5b7e712382a8e4321eddfe462ba21327
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-07-08 15:49:18 +0900

    Added json parsing

---
+++ DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  simplestats
+//
+//  Created by Paul Traylor on 2017/07/08.
+//  Copyright © 2017年 Paul Traylor. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["description"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+    }
+}
--- simplestats/ViewController.swift
+++ simplestats/ViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countdowns = [[String: String]]()
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countdowns.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let countdown = countdowns[indexPath.row]
+        cell.textLabel?.text = countdown["label"]
+        cell.detailTextLabel?.text = countdown["created"]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = countdowns[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "https://tsundere.co/api/countdown"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                parse(json: json)
+            }
+        }
+    }
+    
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let label = result["label"].stringValue
+            let description = result["description"].stringValue
+            let created = result["created"].stringValue
+            let obj = ["label": label, "description": description, "created": created]
+            countdowns.append(obj)
+        }
+        
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
