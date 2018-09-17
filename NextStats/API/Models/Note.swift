14a78a292a77df53e714def63ec3248e821ff9d4
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-09-17 16:27:37 +0900

    Note page stubs

---
+++ NextStats/API/Models/Note.swift
@@ -0,0 +1,48 @@
+//
+//  Note.swift
+//  NextStats
+//
+//  Created by Paul Traylor on 2018/09/17.
+//  Copyright © 2018年 Paul Traylor. All rights reserved.
+//
+
+import Foundation
+
+struct Note: Codable {
+    let timestamp: Date
+    let title: String
+    let description: String
+}
+
+struct NoteResponse: Codable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [Note]
+}
+
+extension Note {
+    static func list(for widget: Widget, limit: Int = 100, completionHandler: @escaping ([Note]) -> Void ) {
+        guard let user = ApplicationSettings.username else { return }
+        guard let pass = ApplicationSettings.password else { return }
+
+        var query = URLComponents(url: ApplicationSettings.baseURL.appendingPathComponent("/api/widget/\(widget.slug)/notes"), resolvingAgainstBaseURL: true)
+        query?.queryItems = [URLQueryItem(name: "limit", value: String(limit))]
+
+        guard let url = query?.url else { return }
+
+        authedRequest(url: url, method: "GET", body: nil, username: user, password: pass, completionHandler:  {_, data in
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                decoder.dateDecodingStrategy = .custom(dateDecode)
+                do {
+                    let notes = try decoder.decode(NoteResponse.self, from: data)
+                    completionHandler(notes.results)
+                } catch let error {
+                    print(error)
+                }
+            }
+        })
+    }
+}
---
+++ NextStats/Views/NoteTableController.swift
@@ -0,0 +1,44 @@
+//
+//  NoteTableController.swift
+//  NextStats
+//
+//  Created by Paul Traylor on 2018/09/17.
+//  Copyright © 2018年 Paul Traylor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NoteTableController: UITableViewController {
+    var notes = [Note]()
+
+    // MARK: - lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let parent = tabBarController as? DetailController else { return }
+        guard let widget = parent.widget else { return }
+        Note.list(for: widget, limit: 500) { (notes) in
+            self.notes = notes.sorted { $0.timestamp > $1.timestamp }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    // MARK: - tableView
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SampleTableCell", for: indexPath)
+        let note = notes[indexPath.row]
+
+        cell.textLabel?.text = note.title
+        cell.detailTextLabel?.text = note.description
+
+        return cell
+    }
+}
